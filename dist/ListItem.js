d9484b832fc5b68adaef9799533cf8d7ad9e7346
Kevin Rademan <kevin@versa.co.za>
2017-07-02 13:16:32 +0200

    Add dist to simplyfy including module

---
+++ dist/ListItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import cx from 'classnames';
+
+let ListItem = React.createClass({
+	getDefaultProps() {
+		return {
+			disabled: false,
+			selected: false,
+			focused: false
+		};
+	},
+	render() {
+		let classes = cx('react-list-select--item', {
+			'is-disabled': this.props.disabled,
+			'is-selected': this.props.selected,
+			'is-focused': this.props.focused
+		});
+
+		return React.createElement(
+			'li',
+			{ className: classes,
+				onMouseOver: () => this.props.onMouseOver(this.props.index),
+				onClick: event => this.props.onChange({ event, index: this.props.index }) },
+			this.props.children
+		);
+	}
+});
+
+export default ListItem;
